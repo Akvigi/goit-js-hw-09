5aa2835afce804397a165a68250cae9a5d727a33
Akvigi <94718260+Akvigi@users.noreply.github.com>
2022-06-18 16:34:41 +0000

    Deploying to gh-pages from @ Akvigi/goit-js-hw-09@f4b647f049292176bd68ee1f7a75d0cef80c9a25 🚀

---
+++ 01-color-switcher.694dce87.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body"),r=null;function o(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16)),t.setAttribute("disabled",!0)}t.addEventListener("click",(function(){return r=setInterval((function(){o()}),1e3)})),e.addEventListener("click",(function(){clearInterval(r),t.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.694dce87.js.map
---
+++ 01-color-switcher.e7e01115.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body");let o=null;function r(){n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,t.setAttribute("disabled",!0)}t.addEventListener("click",(()=>o=setInterval((function(){r()}),1e3))),e.addEventListener("click",(function(){clearInterval(o),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.e7e01115.js.map
